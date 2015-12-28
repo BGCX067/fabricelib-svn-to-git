217b98fd1a1ed9ad515016a36163cb25fa54ce34
Your Name <you@example.com>
2015-12-28 20:14:27 +0600

    init

---
+++ branches/FabriceLib/src/FabriceLib.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : FabriceLib.cpp
+// Author      : Fabrice Lecuyer
+// Version     :
+// Copyright   : N/A
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Branch!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ trunk/FabriceLib/src/FabriceLib.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : FabriceLib.cpp
+// Author      : Fabrice Lecuyer
+// Version     :
+// Copyright   : N/A
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
